1140ec6421796e5ef9fc99049e7396650204ba86
DUA-UMER <161318323+DUA-UMER@users.noreply.github.com>
2024-03-18 22:46:52 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+"use strict";
+let uname = "dua";
+let ulocation = "karachi";
+let marks = 500;
+let percentage = 80;
+console.log("name:", uname);
+console.log("location", ulocation);
+console.log("marks", marks);
+console.log("percentage:", percentage);
---
+++ main.ts
@@ -0,0 +1,10 @@
+let uname ="dua"
+let ulocation : string = "karachi"
+let marks = 500
+let percentage : number = 80
+
+
+console.log("name:",uname)
+console.log("location", ulocation)
+console.log("marks",marks)
+console.log("percentage:", percentage)
